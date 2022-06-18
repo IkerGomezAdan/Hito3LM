2e53809201deba8744cf350f2d1ae234cd92a666
IkerGomezAdan <93380171+IkerGomezAdan@users.noreply.github.com>
2022-06-18 12:57:29 +0200

    Add files via upload

---
+++ HitoLM/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript" src="script2.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/style.css" rel="stylesheet">
+    <title>Lenguaje Maracas</title>
+</head>
+<body>
+    
+    <hr>
+    <h1>BOLETIN DE NOTAS</h1>
+    <div class="container">
+        
+    <input type="button" value="Ver" onclick="datos()" class="ver">
+    <hr>
+    <table id="demo"></table>
+    </div>
+    
+
+</body>
+</html>
